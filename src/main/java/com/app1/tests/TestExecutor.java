cde1bd178adffb6bb5f53aff85b63c40c0341572
Abhishek Sharma <abhipassion.sharma@gmail.com>
2020-10-24 15:49:36 +0530

    Update TestExecutor.java

--- src/main/java/com/app1/tests/TestExecutor.java
+++ src/main/java/com/app1/tests/TestExecutor.java
@@ -19,5 +19,31 @@ public class TestExecutor {
                 }
             }
         });
+        
+        /**
+        ExecutorService execService = Executors.newFixedThreadPool(5);
+        List<Future<Integer>> futResultsList = new ArrayList<Future<Integer>>();
+        for (int i = 0; i < 10; i++) {
+          futResultsList.add(execService.submit(new GenerateRandom()));
+        }
+
+        // System.out.println(futResultsList.get(0).get()); // this future get() is a blocking operation
+
+        // do some other work
+        Thread.sleep(1000);
+
+        // print out all the future results
+        futResultsList.stream()
+            .forEach(e -> {
+              try {
+                System.out.println(e.get());
+              } catch (InterruptedException | ExecutionException exc) {
+                exc.printStackTrace();
+              }
+            });
+
+        // clean up
+        execService.shutdown();
+        **/
     }
 }
